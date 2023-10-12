73e73b8be72914e57f651a49b1d2d055d7978ce2
Konstantin <konstantin@remicel.com>
2023-10-12 17:23:34 +0300

    product-page-styling

--- src/routes/product-details/product-details.component.jsx
+++ src/routes/product-details/product-details.component.jsx
@@ -3,6 +3,7 @@ import { ProductsContext } from '../../contexts/products.context';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Button from '../../components/button/button.component';
 import { CartContext } from '../../contexts/cart.context';
+import "./product-details.styles.scss";
 
 function ProductDetails() {
   const location = useLocation();
@@ -26,46 +27,45 @@ function ProductDetails() {
 
   return (
     <div>
-      {/* if product category is TSHIRTS OR TRACKSUITS*/}
-      {product.category === 'T-Shirts' || product.category === 'tracksuits' ? (
-        <div>
-          <button onClick={() => setSize('S')}>S</button>
-          <button onClick={() => setSize('M')}>M</button>
-          <button onClick={() => setSize('L')}>L</button>
-        </div>
-      ) : null}
-      {/* if product category is SNEAKERS */}
-      {product.category === 'sneakers' ? (
-        <div>
-          <input>TO DO</input>
-        </div>
-      ) : null}
-
       <div className="checkout-item-container">
-        <div className="image-container">
-          <img src={product.imageUrl} alt={`${product.name}`} />
+        <div className="product-page-left">
+            <img className="product-image" src={product.imageUrl} alt={`${product.name}`} />
         </div>
-
-        <span className="name">{product.name}</span>
-        <span className="quantity">
-          <div className="arrow" onClick={removeItemHandler}>
-            &#10094;
+        <div className="product-page-right">
+          <div className="product-name-price">
+            <span className="name bold">{product.name}</span>
+            <span className="price bold">${product.price}</span>
           </div>
-          <span className="value"> {product.quantity || '0'}</span>
-          <div className="arrow" onClick={addItemHandler}>
-            &#10095;
+          {/* if product category is TSHIRTS OR TRACKSUITS*/}
+          {product.category === 'T-Shirts' || product.category === 'tracksuits' ? (
+            <div className="size-selector-holder">
+              <p className="size-selector-label bold">Select Size</p>
+              <button onClick={() => setSize('S')}>S</button>
+              <button onClick={() => setSize('M')}>M</button>
+              <button onClick={() => setSize('L')}>L</button>
+            </div>
+          ) : null}
+          {/* if product category is SNEAKERS */}
+          {product.category === 'sneakers' ? (
+            <div>
+              <input>TO DO</input>
+            </div>
+          ) : null}
+          <div className="product-info-holder">
+            <p className="product-info-title bold">Product Information</p>
+            <p className="product-info">The Robey Performance Shirt in Navy/Sky Blue was developed especially in accordance with the wishes of our prof clubs. The luxurious training shirt is made of material that helps to remove sweat, to stay dry during the training and because of the perfect fit, one can move about in the field freely and feeling comfortable.</p>
           </div>
-        </span>
-        <span className="price">{product.price}</span>
-        <div className="remove-button" onClick={clearItemHandler}>
-          &#10005;
+          <ul>
+            <li>Free Shipping</li>
+          </ul>
         </div>
       </div>
-
-      <Button buttonType="inverted" onClick={addProductToCart}>
-        Add to cart
-      </Button>
-      <Button onClick={goToHome}>Go back to shop</Button>
+      <div className="checkout-buttons">
+        <Button buttonType="inverted" onClick={addProductToCart}>
+          Add to cart
+        </Button>
+        <Button onClick={goToHome}>Go back to shop</Button>
+      </div>
     </div>
   );
 }
